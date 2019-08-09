28f11deb3f903b8ca2b93f11b0dfb17ccd50535a
EvertonHimself <evertondeoliveira@gmail.com>
2019-08-08 21:46:38 -0300

    Create player script
    The player script was created.

---
+++ Unity - Aula 03/Assets/Scripts/Bird.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    // Checks if player is dead or not.
+    private bool isDead = false;
+    // Gets the rigidbody2d attached to this game object.
+    private Rigidbody2D rb2d;
+
+    // The force the player is pushed upwards.
+    public float upForce = 200f;
+    void Start()
+    {
+        // Assign this game object's rigidbody to the rb2d variable.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (isDead == false)
+        {
+            // Checks if player pressed the right click.
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Resets the velocity to zero everytime the player jumps.
+                rb2d.velocity = Vector2.zero;
+                // The x force is zero because the player doesn't move on the x axis.
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+    }
+    
+    // Checks if the player touched something, if so, turns isDead to true.
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+    }
+}
